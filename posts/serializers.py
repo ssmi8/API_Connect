554571d4c376e91a0b6ca103e2fa8fa0a69b4ddf
ssmi8 <smithsteve8@hotmail.com>
2023-01-04 20:19:01 +0000

    add PostList view, list and create a post

--- posts/serializers.py
+++ posts/serializers.py
@@ -29,7 +29,7 @@ class PostsSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = Post
-        fields - [
+        fields = [
             'id', 'owner', 'is_owner', 'profile_id',
             'profile_image', 'created_at', 'updated_at',
             'title', 'content', 'image', 'image_filter'
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework import status, permissions
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Post
+from .serializers import PostsSerializer
 
-# Create your views here.
+
+class Postlist(APIView):
+    serializer_class = PostsSerializer
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly
+    ]
+
+    def get(self, request):
+        posts = Post.objects.all()
+        serializer = PostsSerializer(
+            posts, many=True, context={'request': request}
+        )
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = PostsSerializer(
+            data=request.data, context={'request': request}
+        )
+        if serializer.is_valid():
+            serializer.save(owner=request.user)
+            return Response(
+                serializer.data, status=status.HTTP_201_CREATED
+            )
+        return Response(
+            serializer.data, status=status.HTTP_400_BAD_REQUEST
+        )
