4a1302cd0fe57621981eb9d9144ac4730440384e
ssmi8 <smithsteve8@hotmail.com>
2023-01-25 14:30:04 +0000

    Removal of likes and comments in jobs serializer to fix Value Error

--- jobs/serializers.py
+++ jobs/serializers.py
@@ -8,10 +8,7 @@ class JobSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
-    like_id = serializers.SerializerMethodField()
-    likes_count = serializers.ReadOnlyField()
-    comments_count = serializers.ReadOnlyField()
-
+    
     def validate_image(self, value):
         if value.size > 2 * 1024 * 1024:
             raise serializers.ValidationError('Image size larger than 2MB!')
@@ -29,21 +26,11 @@ class JobSerializer(serializers.ModelSerializer):
         request = self.context['request']
         return request.user == obj.owner
 
-    def get_like_id(self, obj):
-        user = self.context['request'].user
-        if user.is_authenticated:
-            like = Like.objects.filter(
-                owner=user, post=obj
-            ).first()
-            return like.id if like else None
-        return None
-
     class Meta:
         model = Jobs
         fields = [
             'id', 'owner', 'is_owner', 'profile_id',
             'profile_image', 'created_at', 'updated_at',
-            'title', 'image', 'like_id', 'likes_count', 
-            'comments_count', 'description', 'location',
-            'company'
+            'title', 'content', 'image', 'image_filter',
+            'location', 'company'
         ]
